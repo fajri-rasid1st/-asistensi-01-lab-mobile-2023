433d7d48e5a4eb0ad2af7e0ce464404f2b988b4c
HasanAzyumardi <hasanazyumardiazra@gmail.com>
2023-04-30 18:51:48 +0800

    kmpul tugas4

---
+++ H071211076/tugas-praktikum-4/app/src/main/java/com/example/chattingapp/DataSource.java
@@ -0,0 +1,22 @@
+package com.example.chattingapp;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class DataSource {
+
+    public static ArrayList<Chat> chats = new ArrayList<>(
+            Arrays.asList( // revisi
+                    new Chat("M0stPal0ne","yo","69.69","ما فائدته","ما هو لوريم إيبسوم","ok",R.drawable.p1),
+                    new Chat("Boypabl0","ssup","96.96","deez","nuts","DEEZNUTS",R.drawable.p2),
+                    new Chat("SoyPablo","HEY","12.00","ما هو لوريم إيبسوم","SSUP","لا أحد يحب الألم بذاته، يسعى ورائه أو يبتغيه، ببساطة لأنه الألم.",R.drawable.p3),
+                    new Chat("J0j1","pepproni","09.55","ligma","6ft hole","لا أحد يحب الألم بذاته، يسعى ورائه أو يبتغيه، ببساطة لأنه الألم.",R.drawable.p4),
+                    new Chat("RoyPabl0","yo","13.00","nuts","busy","لا أحد يحب الألم بذاته، يسعى ورائه أو يبتغيه، ببساطة لأنه الألم.",R.drawable.p5),
+                    new Chat("Richigga","uo" ,"14.36","ssuo","fire","لا أحد يحب الألم بذاته، يسعى ورائه أو يبتغيه، ببساطة لأنه الألم.",R.drawable.p6),
+                    new Chat("Malek yomama","uo","23.02","wth","busy","لا أحد يحب الألم بذاته، يسعى ورائه أو يبتغيه، ببساطة لأنه الألم.",R.drawable.p7),
+                    new Chat("darren","yo","05.15","wtf","busy","لا أحد يحب الألم بذاته، يسعى ورائه أو يبتغيه، ببساطة لأنه الألم.",R.drawable.p8),
+                    new Chat("blekber","kareem","12.48","siu","SIU","لا أحد يحب الألم بذاته، يسعى ورائه أو يبتغيه، ببساطة لأنه الألم.",R.drawable.p9),
+                    new Chat("speed","SIUU","09.55","SIU","SIUU","لا أحد يحب الألم بذاته، يسعى ورائه أو يبتغيه، ببساطة لأنه الألم.",R.drawable.p10)
+            )
+    );
+}
